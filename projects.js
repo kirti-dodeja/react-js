9b4b9bf19525587fac3104780f1ccc02981f23fd
Kirti Dodeja <67730404+kirti-dodeja@users.noreply.github.com>
2020-07-08 16:57:47 +0530

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,28 @@
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+
+const PROJECTS=[
+    {
+        id:1,
+        title: 'A demo React Application',
+        description: 'A demo react app which I have created using JS and web concepts',
+        link:'https://github.com/15Dkatz/example',
+        image: project1
+    },
+    {
+        id:2,
+        title: 'Online job portal',
+        description: 'An online job portal',
+        link:'https://github.com/15Dkatz/example',
+        image: project2
+    },
+    {
+        id:3,
+        title: 'University project',
+        description: 'University project for final sem',
+        link:'https://github.com/15Dkatz/example',
+        image: project3
+    }
+];
+export default PROJECTS;
---
+++ socialProfiles.js
@@ -0,0 +1,28 @@
+import emailIcon from '../assets/email_icon.png';
+import githublIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+
+const SOCIAL_PROFILES=[
+    {
+        id:1,
+        link:'mailto:kirti.dodeja95@gmail.com',
+        image: emailIcon
+    },
+    {
+        id:2,
+        link:'https://github.com/kirti-dodeja/react-js',
+        image:githublIcon
+    },
+    {
+        id:3,
+        link:'https://www.linkedin.com/in/kirti-dodeja-250010113',
+        image:linkedinIcon
+    },
+    {
+        id:4,
+        link:'https://twitter.com/?lang=en',
+        image:twitterIcon
+    }
+]
+export default SOCIAL_PROFILES;
